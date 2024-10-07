e9dc6c9aaa742a60f08e0b24905908784d157aae
ljg0610 <jggy0627@naver.com>
2024-10-07 23:00:24 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 624 ms, Memory: 51664 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,11 +1,10 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-
         int[][] coord = new int[n][2];
 
         for (int i = 0; i < n; i++) {
@@ -14,19 +13,18 @@ public class Main {
             coord[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(coord, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b){
-                if(b[1] == a[1]){
-                    return a[0] - b[0];
-                }else{
-                    return a[1] - b[1];
-                }
+        Arrays.sort(coord, (a, b) -> {
+            if (a[1] == b[1]) {
+                return Integer.compare(a[0], b[0]);
+            } else {
+                return Integer.compare(a[1], b[1]);
             }
         });
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            System.out.println(coord[i][0] + " " + coord[i][1]);
+            sb.append(coord[i][0]).append(" ").append(coord[i][1]).append("\n");
         }
+        System.out.print(sb);
     }
 }
