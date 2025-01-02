ee512232dc7b6b9e1594bec8f6bf06b999b0d864
ljg0610 <jggy0627@naver.com>
2025-01-02 15:12:12 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1700 ms, Memory: 300752 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,24 +1,30 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.OutputStreamWriter;
 
 public class Main {
+    static final int numSize = 10000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
+        int[] nums = new int[numSize + 1];
 
-        Arrays.sort(arr);
-        StringBuilder sb = new StringBuilder();
-        for(int i : arr) {
-            sb.append(i).append('\n');
+        for (int i = 0; i < n; i++) {
+            nums[Integer.parseInt(br.readLine())]++;
         }
+        br.close();
 
-        System.out.println(sb);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i <= numSize; i++) {
+            while (nums[i] > 0) {
+                bw.write(i + "\n");
+                nums[i]--;
+            }
+        }
+        bw.flush();
+        bw.close();
     }
 }
