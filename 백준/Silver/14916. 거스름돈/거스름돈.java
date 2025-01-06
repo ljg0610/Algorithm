1060a094960b37cc5706933c1b9cf9ffdd587a36
ljg0610 <jggy0627@naver.com>
2025-01-06 20:39:02 +0900

    [Silver V] Title: 거스름돈, Time: 116 ms, Memory: 15820 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        int result = change(n);
+        bw.write(result + "\n");
+
+        br.close();
+        bw.close();
+    }
+
+    static int change(int n) {
+        int result = 0;
+        
+        while (n > 0) {
+            if(n % 5 == 0) {
+                result += n / 5;
+                return result;
+            }
+
+            if(n < 2) {
+                return -1;
+            }
+            n -= 2;
+            result++;
+        }
+
+        return result;
+    }
+}
