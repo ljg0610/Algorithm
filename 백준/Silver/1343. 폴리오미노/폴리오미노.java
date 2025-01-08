581b1643dce7870c7de2bb2922453527465dc9ac
ljg0610 <jggy0627@naver.com>
2025-01-08 20:10:06 +0900

    [Silver V] Title: 폴리오미노, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -1,27 +1,50 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
+        String[] inputs = input.split("\\.");
         br.close();
 
-        poliomino(input);
+        poliomino(input, inputs);
     } 
 
-    private static void poliomino(String input) {
+    private static void poliomino(String input ,String[] inputs) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
         final String A = "AAAA";
         final String B = "BB";
 
-        input = input.replaceAll("XXXX", A);
-        input = input.replaceAll("XX", B);
+        for (int i = 0; i < inputs.length; i++) {
+            int value = inputs[i].length();
 
-        if(input.contains("X")) {
-            System.out.println("-1");
-        } else {
-            System.out.println(input);
+            if (value % 2 != 0) {
+                System.out.println(-1);
+                return;
+            } else if (value % 4 == 0) {
+                sb.append(A.repeat(value / 4));
+            } else {
+                sb.append(A.repeat(value / 4));
+                sb.append(B);
+            }
+
+            if (i != inputs.length - 1) {
+                sb.append(".");
+            }
         }
+
+        if(input.length() - sb.toString().length() != 0) {
+            sb.append(".".repeat(input.length() - sb.toString().length()));
+        }
+        
+        bw.write(sb + "\n");
+        bw.flush();
+        bw.close();
     }
 }
