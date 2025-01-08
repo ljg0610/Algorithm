75a1bafb40e0edb0f73a8ce55c7a032a3c1cd4fb
ljg0610 <jggy0627@naver.com>
2025-01-08 18:05:38 +0900

    [Silver V] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 108 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] visited = new int[101][101];
+    public static void main(String[] args) throws IOException {
+        final int INPUT = 4;
+        
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        for (int i = 0; i < INPUT; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int xLeft = Integer.parseInt(st.nextToken());
+            int yLeft = Integer.parseInt(st.nextToken());
+            int xRight = Integer.parseInt(st.nextToken());
+            int yRight = Integer.parseInt(st.nextToken());
+
+            calcRect(xLeft, yLeft, xRight, yRight);
+        }
+
+        int result = 0;
+        for (int i = 1; i < visited.length; i++) {
+            for (int j = 1; j < visited[i].length; j++) {
+                if(visited[i][j] == 1) {
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    private static void calcRect(int xLeft, int yLeft, int xRight, int yRight) {
+        int row = xRight - xLeft;
+        int col = yRight - yLeft;
+
+        for (int i = xLeft; i < xLeft + row; i++) {
+            for (int j = yLeft; j < yLeft + col; j++) {
+                visited[i][j] = 1;
+            }
+        }
+    }
+}
