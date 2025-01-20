f7f5e1e03635dcf10f8a39c545932fdadd2afaec
ljg0610 <jggy0627@naver.com>
2025-01-20 18:35:26 +0900

    [Silver IV] Title: 다음 소수, Time: 356 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int test = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < test; i++) {
+            long n = Long.parseLong(br.readLine());
+            sb.append(searchDecimal(n)).append('\n');
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    private static long searchDecimal(long n) {
+        if (n == 0 || n == 1) {
+            return 2;
+        }
+
+        while (true) {
+            boolean flag = true;
+            
+            for (long i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    flag = false;
+                    break;            
+                }
+            }   
+
+            if (flag) {
+                return n;
+            }
+
+            n++;
+        }
+    }
+}
