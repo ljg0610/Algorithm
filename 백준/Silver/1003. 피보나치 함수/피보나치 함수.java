44428daccddc4495761906059984db9f5a391804
ljg0610 <jggy0627@naver.com>
2025-01-23 17:57:53 +0900

    [Silver III] Title: 피보나치 함수, Time: 104 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -3,32 +3,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int dp[][] = new int[41][2];
+    static Integer dp[][] = new Integer[41][2];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCase = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
-        getFibonacciCalls();
-
-        for (int i = 0; i < testCase; i++) {
-            int num = Integer.parseInt(br.readLine());
-            sb.append(dp[num][0]).append(" ").append(dp[num][1]).append('\n');
-        }
-
-        System.out.println(sb.toString());
-    }
-
-    private static void getFibonacciCalls() {
         dp[0][0] = 1;
         dp[0][1] = 0;
         dp[1][0] = 0;
         dp[1][1] = 1;
 
-        for (int i = 2; i < 41; i++) {
-            dp[i][0] = dp[i-1][0] + dp[i-2][0];
-            dp[i][1] = dp[i-1][1] + dp[i-2][1];
+        StringBuilder sb = new StringBuilder();
+ 
+        for (int i = 0; i < testCase; i++) {
+            int n = Integer.parseInt(br.readLine());
+            fibonacci(n);
+            sb.append(dp[n][0]).append(' ').append(dp[n][1]).append('\n');
         }
+
+        System.out.println(sb.toString());
+    }
+
+    private static Integer[] fibonacci(int n) {
+        if (dp[n][0] == null  || dp[n][1] == null) {
+            dp[n][0] = fibonacci(n - 1)[0] + fibonacci(n - 2)[0];
+            dp[n][1] = fibonacci(n - 1)[1] + fibonacci(n - 2)[1];
+        }
+
+        return dp[n];
     }
 }
