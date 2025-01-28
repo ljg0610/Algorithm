e1cc4cf19853bb5953faf2fd019e40242ef2ac80
ljg0610 <jggy0627@naver.com>
2025-01-28 21:31:38 +0900

    [level 1] Title: K번째수, Time: 1.16 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,19 +1,15 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
+
         for (int i = 0; i < commands.length; i++) {
-            int[] arr = new int[commands[i][1] - commands[i][0] + 1];
-            int k = 0;
-            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
-                arr[k++] = array[j];
-            }
-            Arrays.sort(arr);
-            answer[i] = arr[commands[i][2] - 1];
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
         }
-        
+
         return answer;
     }
 }
