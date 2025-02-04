18f1d4331ab86ae51ff48bbfcddaeff6cd14962c
ljg0610 <jggy0627@naver.com>
2025-02-04 15:41:04 +0900

    [Silver II] Title: 연속합, Time: 240 ms, Memory: 23992 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        int[] nums = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        System.out.println(sequenceSum(nums));
+    }
+
+    private static int sequenceSum(int[] nums) {
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int max = dp[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(nums[i], dp[i-1] + nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+        
+        return max;
+    }
+}
+
