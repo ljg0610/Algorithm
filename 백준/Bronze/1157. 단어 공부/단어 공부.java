c05c2b49a223e92a9df30ccaf4413ea1a6877462
ljg0610 <jggy0627@naver.com>
2025-02-25 17:55:23 +0900

    [Bronze I] Title: 단어 공부, Time: 260 ms, Memory: 23108 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,31 +1,29 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String input = br.readLine().toUpperCase();
+        int[] countArr = new int[26];
 
-        Map<Character, Integer> strMap = new HashMap<>();
-
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            strMap.put(ch, strMap.getOrDefault(ch, 0) + 1);
+        for (char ch : input.toCharArray()) {
+            countArr[ch - 'A']++;
         }
 
-        int maxValue = Integer.MIN_VALUE;
-        char maxKey = ' ';
-
-        for (Map.Entry<Character, Integer> entry : strMap.entrySet()) {
-            if (entry.getValue() > maxValue) {
-                maxValue = entry.getValue();
-                maxKey = entry.getKey();
-            } else if (entry.getValue() == maxValue) {
-                maxKey = '?';
+        int maxCount = 0;
+        char maxChar = ' ';
+        boolean isDuplicate = false;
+        
+        for (int i = 0; i < 26; i++) {
+            if (countArr[i] > maxCount) {
+                maxCount  = countArr[i];
+                maxChar = (char) (i + 'A');
+                isDuplicate = false;
+            } else if (countArr[i] == maxCount) {
+                isDuplicate = true;
             }
         }
-        
-        System.out.println(maxKey);
+
+        System.out.println(isDuplicate ? '?' : maxChar);
     }
 }
