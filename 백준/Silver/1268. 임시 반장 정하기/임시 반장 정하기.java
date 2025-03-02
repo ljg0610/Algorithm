bb6d6c46bf990dbb390d8fed8dbcf5c610032e75
ljg0610 <jggy0627@naver.com>
2025-03-02 18:53:56 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 324 ms, Memory: 132444 KB -BaekjoonHub

---
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int[][] students = new int[num + 1][6];
+
+        StringTokenizer st;
+
+        for (int i = 1; i <= num; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < 6; j++) {
+                students[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int max = 0;
+        int captain = 1;
+
+        for (int i = 1; i <= num; i++) {
+            Set<Integer> set = new HashSet<>();
+            for (int j = 1; j < 6; j++) {
+                for (int k = 1; k <= num; k++) {
+                    if (students[i][j] == students[k][j] && i != k) {
+                        set.add(k);
+                    }
+                }
+            }
+
+            if (set.size() > max) {
+                max = set.size();
+                captain = i;
+            }
+        }
+
+        System.out.println(captain);
+    }
+}
