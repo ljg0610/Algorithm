4f59ba0b3a07162628541a217388405cc52042a3
ljg0610 <jggy0627@naver.com>
2025-03-03 14:31:53 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 124 ms, Memory: 15892 KB -BaekjoonHub

---
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -0,0 +1,73 @@
+import java.io.*;
+
+public class Main {
+    static char[] vowel = {'a', 'e', 'i', 'o', 'u'};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        
+        while(!(input = br.readLine()).equals("end")) {
+            System.out.print("<" + input + "> ");
+            if (isAcceptable(input)) {
+                System.out.println("is acceptable.");
+            } else {
+                System.out.println("is not acceptable.");
+            }
+        }
+    }
+
+    private static boolean isAcceptable (String input) {
+        return containsVowel(input) && continuousWords(input) && chkSame(input);
+    }
+
+    private static boolean containsVowel(String input) {
+        for (char c : input.toCharArray()) {
+            for (char v : vowel) {
+                if (c == v) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean continuousWords(String input) {
+        if (input.length() < 3) {
+            return true;
+        }
+
+        for (int i = 2; i < input.length(); i++) {
+            boolean first = isVowel(input.charAt(i-2));
+            boolean second = isVowel(input.charAt(i-1));
+            boolean third = isVowel(input.charAt(i));
+
+            if ((first && second && third) || (!first && !second && !third)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean chkSame(String input) {
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i-1) == input.charAt(i)) {
+                if (input.charAt(i) != 'e' && input.charAt(i) != 'o') {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isVowel(char c) {
+        for (char v : vowel) {
+            if (c == v) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
