de796063682129940b5ff93bcfffa3ad17815047
ljg0610 <jggy0627@naver.com>
2025-03-08 16:52:13 +0900

    [Silver V] Title: 중복된 숫자, Time: 1844 ms, Memory: 460964 KB -BaekjoonHub

--- 백준/Silver/15719. 중복된 숫자/중복된 숫자.java
+++ 백준/Silver/15719. 중복된 숫자/중복된 숫자.java
@@ -6,17 +6,19 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        boolean[] arr = new boolean[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < n; i++) {
             int m = Integer.parseInt(st.nextToken());
-            arr[m]++;
-            if (arr[m] != 1) {
+            
+            if (arr[m]) {
                 System.out.println(m);
                 return;
             }
+            
+            arr[m] = true;
         }
     }
 }
