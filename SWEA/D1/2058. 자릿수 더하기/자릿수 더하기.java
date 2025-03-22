6eccbbf802304382bfb637efd58dbaaeaad6ea7a
ljg0610 <jggy0627@naver.com>
2025-03-22 12:59:10 +0900

    [D1] Title: 자릿수 더하기, Time: 69 ms, Memory: 22,144 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
@@ -7,12 +7,10 @@ class Solution {
         int n = Integer.parseInt(br.readLine());
         int sum = 0;
 
-        while (n > 9) {
+        while (n > 0) {
             sum += n % 10;
             n /= 10;
         }
-
-        sum += n;
 
         System.out.println(sum);
     }
