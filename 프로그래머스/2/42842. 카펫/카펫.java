5581d40dedfc179e996f7c7fbfd068a8267f7ed7
ljg0610 <jggy0627@naver.com>
2025-05-15 22:11:02 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 57.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int total = brown + yellow;
+        int sum = brown + yellow;
         
-        for (int h = 3; h <= total; h++) {
-            int w = total / h;
-            if ((w-2) * (h-2) == yellow) {
+        for (int h = 3; h <= sum / 2; h++) {
+            int w = sum / h;
+            if ((w - 2) * (h - 2) == yellow) {
                 return new int[]{w,h};
             }
         }
-        
-        
+
         return null;
     }
 }
