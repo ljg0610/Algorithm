4b61ce023ab67fe835e15c6a93b671a4a0f86fa9
ljg0610 <jggy0627@naver.com>
2025-08-13 14:15:29 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.10 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,10 +1,10 @@
 class Solution {
     public long solution(long n) {
-        long answer = -1;
-        double num = Math.sqrt(n);
-        if (num % 1 == 0) {
-            return (long)Math.pow((num + 1), 2);
+        long num = (long)Math.sqrt(n);
+        if (num * num == n) {
+            long next = num + 1;
+            return next * next;
         }
-        return answer;
+        return -1;
     }
 }
