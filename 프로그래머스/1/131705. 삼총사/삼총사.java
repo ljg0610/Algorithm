da9c393f44c5c6dfee8ece0f99cc42c2162a43f4
ljg0610 <jggy0627@naver.com>
2025-08-20 16:12:03 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,17 +1,15 @@
 class Solution {
     public int solution(int[] number) {
-        int result = 0;
-        
-        for (int i = 0; i < number.length - 2; i++) {
-            for (int j = i + 1; j < number.length - 1; j++) {
+        int answer = 0;
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
                 for (int k = j + 1; k < number.length; k++) {
                     if (number[i] + number[j] + number[k] == 0) {
-                        result++;
+                        answer++;
                     }
                 }
             }
         }
-        
-        return result;
+        return answer;
     }
 }
