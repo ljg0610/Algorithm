6288dec432d8ca2eae06814a8c888296a1327a8d
ljg0610 <jggy0627@naver.com>
2025-08-20 16:05:03 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,14 +1,8 @@
 class Solution {
     public int solution(int n) {
-        String start = Integer.toString(n, 3);
-        String tmp = new StringBuilder(start).reverse().toString();
-        int answer = 0;
+        String s = Integer.toString(n, 3);
+        String tmp = new StringBuilder(s).reverse().toString();
         
-        int idx = 0;
-        for (int i = tmp.length() - 1; i >= 0; i--) {
-            answer += Math.pow(3, idx++) * Integer.parseInt(tmp.substring(i, i + 1));
-        }
-        
-        return answer;
+        return Integer.parseInt(tmp, 3);
     }
 }
