3531aef29929ef8320aefe45e7f5461f557c0d72
ljg0610 <jggy0627@naver.com>
2025-08-22 20:56:20 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 60.73 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -3,14 +3,18 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         
         for (int i = 1; i < food.length; i++) {
-            int amount = food[i];
-            for (int j = 0; j < amount / 2; j++) {
+            int num = food[i] / 2;
+            for (int j = 0; j < num; j++) {
                 sb.append(i);
             }
         }
         
-        String nSb = new StringBuilder(sb).reverse().toString();
+        sb.append('0');
         
-        return sb.append(0).append(nSb).toString();
+        for (int i = sb.toString().length() - 2; i >= 0; i--) {
+            sb.append(sb.toString().charAt(i));
+        }
+        
+        return sb.toString();
     }
 }
