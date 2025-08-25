a2c2317ee9ab36daa7e6c59e187e6e6e644b1f7b
ljg0610 <jggy0627@naver.com>
2025-08-25 16:13:29 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,22 +1,16 @@
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        boolean[] flags = new boolean[goal.length];
-        int idx = 0;
         int idx1 = 0;
         int idx2 = 0;
         
         for (String g : goal) {
             if (idx1 < cards1.length && g.equals(cards1[idx1])) {
                 idx1++;
-                flags[idx++] = true;
             } else if (idx2 < cards2.length && g.equals(cards2[idx2])) {
                 idx2++;
-                flags[idx++] = true;
+            } else {
+                return "No";
             }
-        }
-        
-        for (int i = 0; i < goal.length; i++) {
-            if(!flags[i]) return "No";
         }
         
         return "Yes";
