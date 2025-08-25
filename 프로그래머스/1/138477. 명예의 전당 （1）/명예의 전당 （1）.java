48fb3d83b0e1c58e3df2e17efdb8e29c7f0fcef6
ljg0610 <jggy0627@naver.com>
2025-08-25 14:07:03 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.87 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,22 +3,26 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        List<Integer> honorList = new ArrayList<>();
+        int m = Math.min(k, score.length);
+        int[] nums = new int[m];
         
-        for (int i = 0; i < score.length; i++) {
-            int num = score[i];
-            
-            honorList.add(num);
-            Collections.sort(honorList);
-            Collections.reverse(honorList);
-            
-            if (honorList.size() < k) {
-                answer[i] = honorList.get(honorList.size() - 1);
-            } else {
-                answer[i] = honorList.get(k-1);
-            }
-            
+        nums[0] = score[0];
+        answer[0] = nums[0];
+        for (int i = 1 ; i < m; i++) {
+            nums[i] = score[i];
+            Arrays.sort(nums, 0, i + 1);
+            answer[i] = nums[0];
         }
+        
+        
+        for (int i = m; i < score.length; i++) {
+            if (nums[0] < score[i]) {
+                nums[0] = score[i];
+                Arrays.sort(nums);  
+            } 
+            answer[i] = nums[0];
+        }
+        
         return answer;
     }
 }
