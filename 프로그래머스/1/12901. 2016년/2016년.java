9bb1a93ebdc0703e7dbd0880a13beed147a6f522
ljg0610 <jggy0627@naver.com>
2025-08-26 22:49:00 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,15 +1,16 @@
 class Solution {
     public String solution(int a, int b) {
-        int[] days = new int[] {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String[] day = new String[] {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
-        
-        int sum = 0;
-        for (int i = 1; i < a; i++) {
-            sum += days[i];
+        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int days = 0;
+        for (int i = 0; i < a - 1; i++) {
+            days += month[i];
         }
-        sum += b;
         
-        String answer = day[(sum - 1) % 7];
+        days += b - 1;
+        days %= 7;
+        
+        String answer = day[days];
         return answer;
     }
 }
