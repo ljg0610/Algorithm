9b9e5c64c1f35156d26e45a54ce86b3cb20483d1
ljg0610 <jggy0627@naver.com>
2025-08-27 11:26:25 +0900

    [level 1] Title: 과일 장수, Time: 385.91 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -2,19 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        List<Integer> scoreList = new ArrayList<>();
-        for (int s : score) {
-            scoreList.add(s);
+        int answer = 0;
+        int len = score.length;
+        Integer[] scores = new Integer[len];
+        
+        for (int i = 0; i < len; i++) {
+            scores[i] = score[i];
         }
         
-        Collections.sort(scoreList, Collections.reverseOrder());
+        Arrays.sort(scores, Collections.reverseOrder());
         
-        int result = 0;
-        for (int i = 0; i + m <= scoreList.size(); i += m) {
-            int min = scoreList.get(i + m - 1);
-            result += min * m;
+        for (int i = 0; i + m <= len; i+= m) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < m; j++) {
+                if (min > scores[i + j]) {
+                    min = scores[i + j];
+                    if (scores[i + j] > k) {
+                        min = k;
+                    }
+                }
+            }
+            answer += min * m;
+            
+            if (i + m > len) return answer;
         }
         
-        return result;
+        return answer;
     }
 }
