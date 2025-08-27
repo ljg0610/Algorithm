dc304af727b968b5e76f9cfcb4495296f9bd7f51
ljg0610 <jggy0627@naver.com>
2025-08-27 10:40:06 +0900

    [level 1] Title: 덧칠하기, Time: 1.89 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,22 +1,14 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        boolean[] bSection = new boolean[n + 1];
+        int painted = 0;
         
-        for (int idx : section) {
-            bSection[idx] = true;
-        }
-        
-        for (int i = 1; i <= n; i++) {
-            if (bSection[i]) {
-                int range = i + m - 1 <= n ? i + m - 1 : n;
-                for (int j = i; j <= range; j++) {
-                    bSection[j] = false;
-                }
+        for (int pos : section) {
+            if (pos > painted) {
                 answer++;
+                painted = pos + m - 1;
             }
         }
-        
         return answer;
     }
 }
